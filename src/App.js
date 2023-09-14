d3d2c2bbe9ac286f2950117b92ee1621f8eac55f
Austin <austin.amaral3@gmail.com>
2023-09-13 18:08:11 -0700

    done

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import { Routes, Route, Link} from 'react-router-dom';
 import Login from './components/login';
 import FriendsList from './components/friendslist';
 import AddFriend from './components/addfriend';
+import Logout from './components/logout';
+
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <h1>Friends DataBase </h1>
-        <Link className="link" to="/login">Login.</Link>
-        <Link className="link" to="/addfriend">AddFriend.</Link>
-        <Link className="link" to="/friendslist">FriendsList.</Link>
-        <Link className="link" to="/logout">Logout.</Link>
+      <header>
+        <h2>Friends DataBase </h2>
+          <Link className="link" to="/login">Login.</Link>
+          <Link className="link" to="/addfriend">AddFriend.</Link>
+          <Link className="link" to="/friendslist">FriendsList.</Link>
+          <Link className="link" to="/logout">Logout.</Link>
+      </header>
+
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/friendslist" element={<FriendsList />} />
         <Route path="/addfriend" element={<AddFriend />} />
-        <Route path="/logout" element={<Login />} />
+        <Route path="/logout" element={<Logout />} /> 
       </Routes>
     </div>
   );
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Navigate } from "react-router-dom";
+
+const PrivateRouteNow = ({component: Component, ...rest}) => {
+    return <Route {...rest} render={(props) => {
+        if (localStorage.getItem("token")) {
+            return (<Component {...props} />)
+        } else {
+            return (<Navigate to="/login" />)
+        }
+    }}/>
+}
+
+export default PrivateRouteNow;
